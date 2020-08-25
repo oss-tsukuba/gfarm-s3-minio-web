51ec1bc9959da2c5cb9fd98f9b7589da4acd2335
Shingo Nishioka <nis@soum.co.jp>
2020-08-25 13:30:49 +0900

    fix

--- webui/gfarm-s3/console/views.py
+++ webui/gfarm-s3/console/views.py
@@ -7,6 +7,10 @@ import os
 from subprocess import Popen, PIPE
 import sys
 
+def debug_print(s):
+    with open("/tmp/views.log", "a") as f:
+        f.write(s)
+
 def index(request):
     return render(request, "console/index.html", {})
 
@@ -14,7 +18,13 @@ def application(request):
     return render(request, "console/application.html", {})
 
 def launch(request):
-    result_s = cmd(request.POST["username"], request.POST["passwd"])
+    debug_print("{}\n".format(request.POST))
+    username = request.POST["username"]
+    passwd = request.POST["passwd"]
+    action = request.POST["action"]
+    debug_print("username = {}\npasswd = {}\naction = {}\n".format(username, passwd, action))
+    result_s = cmd(username, passwd, action)
+    debug_print("result_s = {}\n".format(result_s))
     result = json.loads(result_s)
     request.session["result"] = result 
     return HttpResponseRedirect(reverse("result"))
@@ -24,12 +34,15 @@ def result(request):
     result = request.session["result"]
     return render(request, "console/result.html", result)
 
-def cmd(username, passwd):
+def cmd(username, passwd, action):
     GFARM_S3_BIN = "/home/user1/work/gfarm-s3-minio-web/bin"
-    action = "stop"
-    action = "start"
+    #action = "stop"
+    #action = "start"
+    #action = "restart"
+    #action = "show"
+    #action = "genkey"
     gfarm_s3_login = os.path.join(GFARM_S3_BIN, "gfarm-s3-login")
-    cmd = [gfarm_s3_login , action, username, passwd]
+    cmd = [gfarm_s3_login, action, username, passwd]
     env = {
         "GLOBUS_GSSAPI_NAME_COMPATIBILITY": "HYBRID",
         #"HOME": os.environ["HOME"],
@@ -44,10 +57,11 @@ def cmd(username, passwd):
         stdout, stderr = p.communicate()
         ret = p.wait()
     except:
-        d = {"status": "ERROR 1", "stderr": stderr.decode()}
-        return json.dumps(d)
+        debug_print("ERROR 1\nSTDERR = {}\n".format(stderr.decode()))
+        return json.dumps({"status": "ERROR 1"})
     if ret == 0:
+        debug_print("SUCCESS\nSTDERR = {}\n".format(stderr.decode()))
         return stdout.decode().strip()
     else:
-        d = {"status": "ERROR 2", "stderr": stderr.decode()}
-        return json.dumps(d)
+        debug_print("ERROR 2\nSTDERR = {}\n".format(stderr.decode()))
+        return json.dumps({"status": "ERROR 2"})
