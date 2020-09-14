02fa6b20a8f7040a8fa95176701cccf185538917
Shingo Nishioka <nis@soum.co.jp>
2020-09-14 16:46:24 +0900

    fix

--- webui/gfarm-s3/console/cmd.py
+++ webui/gfarm-s3/console/cmd.py
@@ -13,18 +13,29 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def cmd(username, passwd, action):
-    #GFARM_S3_BIN = "/home/user1/work/gfarm-s3-minio-web/bin"
-    GFARM_S3_BIN = "/usr/local/bin"
-    #action = "stop" | "start" | "restart" | "show" | "genkey"
-    gfarm_s3_login = os.path.join(GFARM_S3_BIN, "gfarm-s3-login")
-    cmd = [gfarm_s3_login, action, username, passwd]
-
+def gfarm_s3_login(action, username, passwd, stdin = None, authenticated = None, bucket = None):
 ### XXX debug
-    #logger.debug("CMD = {}".format(cmd))
+    GFARM_S3_BIN = "/usr/local/bin"
+    args = [action, username, passwd]
+    if authenticated is not None:
+        args = ["--authenticated", authenticated] + args
+    if bucket is not None:
+        args = ["--bucket", bucket] + args
+    gfarm_s3_login_bin = os.path.join(GFARM_S3_BIN, "gfarm-s3-login")
+    #logger.debug("BIN = {}".format(gfarm_s3_login_bin))
+    #logger.debug("ARGS = {}".format(args))
+    return Popen([gfarm_s3_login_bin] + args, stdin = stdin, stdout = PIPE, stderr = PIPE, env = {})
+
+def cmd(action, username, passwd, authenticated = None):
+    #GFARM_S3_BIN = "/home/user1/work/gfarm-s3-minio-web/bin"
+    #action = "stop" | "start" | "restart" | "show" | "genkey"
+    #if authenticated is not None:
+        #gfarm_s3_login_args = [action, username, ""]
+    #elif passwd is not None:
+        #gfarm_s3_login_args = [action, username, passwd]
 
     try:
-        p = Popen(cmd, stdout = PIPE, stderr = PIPE, env = {})
+        p = gfarm_s3_login(action, username, passwd, authenticated = authenticated)
     except:
         logger.debug("ERROR 1")
         return {"status": "ERROR 1", "reason": "Popen failed"}
@@ -43,49 +54,11 @@ def cmd(username, passwd, action):
         result["expiration_date_calendar_datetime"] = time.ctime(result["expiration_date"])
     return result
 
-def get_group_list(username):
-    cmd = ["sudo", "-u", username, "/usr/local/bin/gfgroup"]
-
-### XXX debug
-    #logger.debug("CMD = {}".format(cmd))
-
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE, env = {})
-
-    stdout, stderr = p.communicate()
-
-    p.wait()
-
-### XXX debug
-    #logger.debug("STDOUT = {}".format(stdout))
-
-    return stdout.decode().split('\n')
-
-def get_user_list(username):
-    cmd = ["sudo", "-u", username, "/usr/local/bin/gfuser", "-l"]
-
-### XXX debug
-    #logger.debug("CMD = {}".format(cmd))
-
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE, env = {})
-
-    stdout, stderr = p.communicate()
-
-    p.wait()
-
-### XXX debug
-    #logger.debug("STDOUT = {}".format(stdout))
-
-    return stdout.decode().split('\n')
-    #return ["a", "b", "c"]
-
 def get_bucket_list(username):
-    s3rootdir = "/home/hp120273/hpci005858/tmp/gfarms3/hpci005858"
-    cmd = ["sudo", "-u", username, "/usr/local/bin/gfls", s3rootdir]
+    ###s3rootdir = "/home/hp120273/hpci005858/tmp/gfarms3/hpci005858"
+    ###cmd = ["sudo", "-u", username, "/usr/local/bin/gfls", s3rootdir]
 
-### XXX debug
-    #logger.debug("CMD = {}".format(cmd))
-
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE, env = {})
+    p = gfarm_s3_login("gfls", username, "", authenticated = "unspecified")
 
     stdout, stderr = p.communicate()
 
@@ -94,17 +67,14 @@ def get_bucket_list(username):
 ### XXX debug
     #logger.debug("STDOUT = {}".format(stdout))
 
-    return stdout.decode().split('\n')
+    return [e for e in stdout.decode().split('\n') if e != ""]
 
 def get_bucket_acl(username, bucket):
-    s3rootdir = "/home/hp120273/hpci005858/tmp/gfarms3/hpci005858"
-    bucketpath = os.path.join(s3rootdir, bucket)
-    cmd = ["sudo", "-u", username, "/usr/local/bin/gfgetfacl", bucketpath]
+    ##s3rootdir = "/home/hp120273/hpci005858/tmp/gfarms3/hpci005858"
+    ##bucketpath = os.path.join(s3rootdir, bucket)
+    ##cmd = ["sudo", "-u", username, "/usr/local/bin/gfgetfacl", bucketpath]
 
-### XXX debug
-    #logger.debug("CMD = {}".format(cmd))
-
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE, env = {})
+    p = gfarm_s3_login("gfgetfacl", username, "", authenticated = "unspecified", bucket = bucket)
 
     stdout, stderr = p.communicate()
 
@@ -113,25 +83,84 @@ def get_bucket_acl(username, bucket):
 ### XXX debug
     #logger.debug("STDOUT = {}".format(stdout))
 
-    return stdout.decode().split('\n')
+    return stdout.decode()
+
+def set_bucket_acl(username, bucket, postdata):
+#    logger.debug("SET BUCKET ACL: {} {}".format(username, bucket))
+#    logger.debug("SET BUCKET ACL: postdata = {}".format(postdata))
+#    for key, value in postdata.items():
+#        logger.debug("SET BUCKET ACL: {} = {}".format(key, value))
+    v = [postdata[e.replace("opt", "sel")] + ":" + postdata[e] for e in postdata if e.startswith("opt:")]
+
+    acl_1_string = postdata["acl_1_string"]
+    acl_2_edited = "\n".join(v)
+
+    logger.debug("acl_1_string: {}".format(acl_1_string))
+    logger.debug("acl_2_edited: {}".format(acl_2_edited))
+
+    acl_new = acl_1_string + acl_2_edited 
+
+    p = gfarm_s3_login("gfsetfacl", username, "", authenticated = "unspecified", bucket = bucket, stdin = PIPE)
+
+### + "user:hpci001971:rwx\n"
+
+    stdout, stderr = p.communicate(input = acl_new.encode())
+
+    p.wait()
+
+### XXX debug
+#    logger.debug("STDOUT = {}".format(stdout))
+#    logger.debug("STDERR = {}".format(stderr))
+
+    return stdout.decode()
+
+def get_group_list(username):
+    ###cmd = ["sudo", "-u", username, "/usr/local/bin/gfgroup"]
+    ###gfarm_s3_login_args = ["--authenticated", authenticated, "gfgroup", username, ""]
+
+    p = gfarm_s3_login("gfgroup", username, "", authenticated = "unspecified")
+
+    stdout, stderr = p.communicate()
+
+    p.wait()
+
+### XXX debug
+    #logger.debug("STDOUT = {}".format(stdout))
+
+    return [e for e in stdout.decode().split('\n') if e != ""]
+
+def get_user_list(username):
+    ###cmd = ["sudo", "-u", username, "/usr/local/bin/gfuser", "-l"]
+
+    p = gfarm_s3_login("gfuser", username, "", authenticated = "unspecified")
+
+    stdout, stderr = p.communicate()
+
+    p.wait()
+
+### XXX debug
+    #logger.debug("STDOUT = {}".format(stdout))
+
+    return [e for e in stdout.decode().split('\n') if e != ""]
+    #return ["a", "b", "c"]
+
+def get_groups_users_list(username):
+    groups = get_group_list(username)
+    groups.sort()
+    groups = [{"id": "group:" + e, "text": e} for e in groups]
+
+    users = get_user_list(username)
+    users.sort()
+    users = [{"id": "user:" + user_id(e), "text": pretty_name(e)} for e in users]
+
+    return (groups, users)
+
+def user_id(s):
+    ss = s.split(':')
+    return ss[0]
 
 def pretty_name(s):
     ss = s.split(':')
     uu = ss[1].split('[')
     t = urllib.parse.unquote(uu[0]) + "(" + ss[0] + ")"
     return t
-
-def user_id(s):
-    ss = s.split(':')
-    return ss[0]
-
-def get_users_groups_list(username):
-    users = [e for e in get_user_list(username) if e != ""]
-    users.sort()
-    users = [{"id": "user:" + user_id(e), "text": pretty_name(e)} for e in users]
-
-    groups = [e for e in get_group_list(username) if e != ""]
-    groups.sort()
-    groups = [{"id": "group:" + e, "text": e} for e in groups]
-
-    return (groups, users)
--- webui/gfarm-s3/console/views.py
+++ webui/gfarm-s3/console/views.py
@@ -13,19 +13,20 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-### session["status"]:   "success"   login succeeded
-###                      "error"     login failed
-###                      no-key      login never tried
+### session["global_username"]:
+###   "non-empty"  login succeeded
+###   ""           login failed
+###   no-key       login never tried
 
 def is_login_failed(session):
-    if session is None or not "status" in session:
+    if session is None or not "global_username" in session:
         return False
-    return session["status"] != "success"
+    return session["global_username"] == ""
 
 def need_login(session):
-    if session is None or not "status" in session:
+    if session is None or not "global_username" in session:
         return True
-    return session["status"] != "success"
+    return session["global_username"] == ""
 
 def index(request):
     logger.debug("@@@ session = {}\n".format(request.session))
@@ -40,52 +41,88 @@ def login(request):
     elif request.method == 'POST':
 
         logger.debug("request.method = {}".format(request.method))
-    
+ 
         logger.debug("request.POST = {}".format(request.POST))
-    
+ 
         username = request.POST["username"]
         passwd = request.POST["passwd"]
         logger.debug("username = {} --- passwd = {}".format(username, passwd))
-        cmd_result = cmd.cmd(username, passwd, "start")
+        cmd_result = cmd.cmd("start", username, passwd)
         if cmd_result is None or cmd_result["status"] != "success":
-            request.session["status"] = "error"
+            request.session["global_username"] = ""
             return HttpResponseRedirect(reverse("login"))
         logger.debug("cmd_result = {}".format(cmd_result))
-        request.session["cmd_result"] = cmd_result 
-        request.session["status"] = "success" 
-        request.session["username"] = username
+        request.session["global_username"] = username
+        request.session["authenticated_method"] = cmd_result["authenticated_method"]
         return HttpResponseRedirect(reverse("result"))
 
 def result(request):
     if need_login(request.session):
         return HttpResponseRedirect(reverse("login"))
-    cmd_result = request.session["cmd_result"]
+    username = request.session["global_username"]
+    authenticated = request.session["authenticated_method"]
+    cmd_result = cmd.cmd("info", username, "", authenticated = authenticated)
+    #cmd_result = request.session["cmd_result"]
+    #cmd_result = {}
+    logger.debug("cmd_result = {}".format(cmd_result))
     return render(request, "console/result.html", cmd_result)
 
 def list(request):
     if need_login(request.session):
         return HttpResponseRedirect(reverse("login"))
-    username = "user1"
+    username = request.session["global_username"]
     bucketlist = cmd.get_bucket_list(username)
     return render(request, "console/list.html", {"bucketlist": bucketlist})
+
+def is_metainfo(s):
+    return s.startswith("#")
+
+def is_unmodifiable(s):
+    return not is_metainfo(s) and ("::" in s or s.startswith("default:"))
+
+def is_modifiable(s):
+    return not is_metainfo(s) and "::" not in s and not s.startswith("default:")
+
+def edict(e):
+    r = e.split(":")
+    return {"id": r[0] + ":" + r[1], "perm": r[2]}
 
 def aclfile(request):
     if need_login(request.session):
         return HttpResponseRedirect(reverse("login"))
+
+    username = request.session["global_username"]
+
     if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        logger.debug("request.POST = {}".format(request.POST))
-        for key, value in request.POST.items():
+        logger.debug("request.GET = {}".format(request.GET))
+        for key, value in request.GET.items():
             logger.debug("{} = {}".format(key, value))
+        bucket = request.GET["bucket"]
+    elif request.method == 'POST':
+#        logger.debug("request.POST = {}".format(request.POST))
+#        for key, value in request.POST.items():
+#            logger.debug("{} = {}".format(key, value))
+#
+        bucket = request.POST["bucket"]
+        cmd.set_bucket_acl(username, bucket, request.POST)
 
-
-
-    username = "user1"
-    bucket_acl = cmd.get_bucket_acl(username, "mybucket")
-    metainfo = [e for e in bucket_acl if e.startswith("#")]
-    bucket_acl = [e for e in bucket_acl if not e.startswith("#") and e != ""]
-    (groups, users) = cmd.get_users_groups_list(username)
+    original_bucket_acl = cmd.get_bucket_acl(username, bucket)
+    bucket_acl_split = [e for e in original_bucket_acl.split('\n') if e != ""]
+    acl_0 = [e for e in bucket_acl_split if is_metainfo(e)]
+    acl_1 = [e for e in bucket_acl_split if is_unmodifiable(e)]
+    acl_2 = [e for e in bucket_acl_split if is_modifiable(e)]
+    acl_2_split = [edict(e) for e in acl_2]
+    (groups, users) = cmd.get_groups_users_list(username)
     #logger.debug("groups = {}".format(groups))
     #logger.debug("users = {}".format(users))
-    return render(request, "console/aclfile.html", {"bucket_acl": bucket_acl, "metainfo": metainfo, "groups_users": groups + users})
+    logger.debug("acl_0 = {}".format(acl_0))
+    logger.debug("acl_1 = {}".format(acl_1))
+    logger.debug("acl_2 = {}".format(acl_2))
+    dict = {"bucket": bucket,
+            "acl_0": acl_0,
+            "acl_1": acl_1,
+            "acl_2": acl_2,
+            "acl_2_split": acl_2_split,
+            "groups_users": groups + users,
+            "acl_1_string": "\n".join(acl_1)}
+    return render(request, "console/aclfile.html", dict)
