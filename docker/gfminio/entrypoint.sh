13795661584f53a1daf7c20217bff966f06543ec
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2021-11-09 21:51:15 +0900

    store S3 secret access keys permanently in docker volume.

--- docker/gfminio/entrypoint.sh
+++ docker/gfminio/entrypoint.sh
@@ -16,6 +16,7 @@ set -eux -o pipefail
 HOME_BASE=/home
 HOST_HOME_BASE=/host_home
 USERMAP=/gfarm-s3-usermap.conf
+SECRET_VOLUME=/s3secret
 
 ### TODO master
 #GFARM_S3_MINIO_WEB_BRANCH=router
@@ -66,7 +67,8 @@ install_gf_s3() {
 SPOOL_DIR=/var/spool
 MINIO_INIT_FLAG_PATH=${SPOOL_DIR}/minio
 if [ ! -f ${MINIO_INIT_FLAG_PATH} ]; then
-    install_gf_s3
+    time install_gf_s3
+    echo "gfarm s3 installation completed"
 
     sed -i -e 's/^NAME_COMPATIBILITY=STRICT_RFC2818$/NAME_COMPATIBILITY=HYBRID/' /etc/grid-security/gsi.conf
 
@@ -132,6 +134,15 @@ for line in $(cat "${USERMAP}"); do
     GFARM2RC="${HOMEDIR}/.gfarm2rc"
     HOST_GFARM2RC="${HOST_HOMEDIR}/.gfarm2rc"
     mksym "${HOST_GFARM2RC}" "${GFARM2RC}"
+
+    ### .gfarm-s3/
+    SECRET_DIR="${HOMEDIR}/.gfarm-s3"
+    SECRET_DIR_VOLUME="${SECRET_VOLUME}/${LOCAL_USERNAME}"
+    mkdir -p "${SECRET_DIR_VOLUME}"
+    chmod 700 "${SECRET_DIR_VOLUME}"
+    [ -d "${SECRET_DIR_VOLUME}" ] || echo "WARNING: ${SECRET_DIR_VOLUME} is not a directory"
+    chown "${LOCAL_USERNAME}":root "${SECRET_DIR_VOLUME}"
+    mksym "${SECRET_DIR_VOLUME}" "${SECRET_DIR}"
 
     if [ -n "${MYPROXY_SERVER}" ]; then
         MYPROXY_LOGON="${HOMEDIR}/myproxy-logon"
